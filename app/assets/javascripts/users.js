e9eee52295a4859e9b3e66e62d63707e5171236e
Bill Shannon <bill@billshannon.com>
2015-03-03 20:28:05 -0700

    Fixed stripe JS errors

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,17 +1,14 @@
-/**
- * Created by was3 on 3/2/15.
- */
-$(document).ready(function() {
+$(document).ready(function () {
     Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
     // Watch for form submission:
-    $("#form-submit-btn").click(function(event) {
+    $("#form-submit-btn").click(function (event) {
         event.preventDefault();
         $('input[type=submit]').prop('disabled', true);
         var error = false;
-        var ccNum = $('#card_number').val();
-        cvcNum = $('#card_code').val();
-        expMonth = $('#card_month').val();
-        expYear = $('#card_year').val();
+        var ccNum = $('#card_number').val(),
+            cvcNum = $('#card_code').val(),
+            expMonth = $('#card_month').val(),
+            expYear = $('#card_year').val();
 
         if (!error) {
             // Get the Stripe token:
@@ -23,19 +20,19 @@ $(document).ready(function() {
             }, stripeResponseHandler);
         }
         return false;
-    }), // form submission
+    }); // form submission
 
-        function stripResponseHandler(status, response) {
-            // Get a reference to the form:
-            var f = $("#new_user");
+    function stripeResponseHandler(status, response) {
+        // Get a reference to the form:
+        var f = $("#new_user");
 
-            // Get the token from the response:
-            var token = response.id;
+        // Get the token from the response:
+        var token = response.id;
 
-            // Add the token to the form
-            f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+        // Add the token to the form
+        f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
 
-            // Submit the form:
-            f.get(0).submit();
-        }
+        // Submit the form:
+        f.get(0).submit();
+    }
 });
