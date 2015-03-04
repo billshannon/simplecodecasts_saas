9341ca984102f50a57a4fcd1dc270482dbf32141
Bill Shannon <bill@billshannon.com>
2015-03-03 20:51:55 -0700

    Fixed typos

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,9 @@
 class Contact < ActiveRecord::Base
-  validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: true
+  validates :name,  presence: true
+  # , length: { maximum: 50 }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true
+  #, length: { maximum: 255 },
+  # format: { with: VALID_EMAIL_REGEX },
+  #          uniqueness: true
 end
