6016598b897bb03c6bd06b880b24ddaf66954cf3
Bill Shannon <bill@billshannon.com>
2015-03-06 23:30:51 -0700

    Added bare bones user show page

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,10 +1,10 @@
 class Profile < ActiveRecord::Base
 belongs_to :user
 
-before_save { self.email = email.downcase }
-validates :first_name, :last_name, presence: true, length: { maximum: 50 }
-VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-validates :email, presence: true, length: { maximum: 255 },
-          format: { with: VALID_EMAIL_REGEX },
-          uniqueness: { case_sensitive: false }
+# before_save { self.email = email.downcase }
+# validates :first_name, :last_name, presence: true, length: { maximum: 50 }
+# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+# validates :email, presence: true, length: { maximum: 255 },
+#           format: { with: VALID_EMAIL_REGEX },
+#           uniqueness: { case_sensitive: false }
 end
